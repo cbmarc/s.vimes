059eaad82b7d6f7c79c8aa2efcb6f8d2713e84e2
Marc Carné <marc.carne@werlabs.com>
2019-03-08 01:57:33 +0100

    Remove logger messages

--- run-nightwatch.sh
+++ run-nightwatch.sh
@@ -1,10 +1,7 @@
-echo "Starting nightwatch"
 rm -rf /home/node/videos/*
 rm -rf /home/node/screenshots/*
 rm -rf /opt/node/videos/*
 rm -rf /opt/node/screenshots/*
 nightwatch "$@" 
-echo "Copying videos and screenshots."
 cp -R /home/node/videos/* /opt/node/videos/
 cp -R /home/node/screenshots/* /opt/node/screenshots/
-echo "Finished copying videos and screenshots."
