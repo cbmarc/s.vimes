92a3ce5a0f31878f8cc088b0e87bb9d13cac873c
Marc Carné <marc.carne@werlabs.com>
2019-03-08 02:19:03 +0100

    Redirect npm install to dev null

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # shellcheck disable=SC2086
 cp -R /opt/node/* /home/node/
 chown node:node /home/node/
-npm install
+npm install > /dev/null
 
 exec wait-for --timeout "${WAIT_FOR_TIMEOUT:-10}" $WAIT_FOR_HOSTS -- \
     run-nightwatch   
