e7cc51d90251276b130bbfa2c5768372da34292d
Marc Carné <marc.carne@werlabs.com>
2019-03-12 15:54:00 +0100

    Cleanup videos before execution.

--- run-nightwatch.sh
+++ run-nightwatch.sh
@@ -1,5 +1,5 @@
-#rm -rf /home/node/videos/*
-#rm -rf /home/node/screenshots/*
+rm -rf /home/node/videos/*
+rm -rf /home/node/screenshots/*
 nightwatch "$@" 
 cp -R /home/node/videos/* /opt/node/videos/ 2>/dev/null
 cp -R /home/node/screenshots/* /opt/node/screenshots/ 2>/dev/null
