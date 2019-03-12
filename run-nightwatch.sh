984d7e011efb61ebb6e1131ff0a5d4f33dd0914e
Marc Carné <marc.carne@werlabs.com>
2019-03-12 16:33:43 +0100

    Fix bug. Execution was not returning nightwatch result.

--- run-nightwatch.sh
+++ run-nightwatch.sh
@@ -1,5 +1,7 @@
 rm -rf /home/node/videos/*
 rm -rf /home/node/screenshots/*
-nightwatch "$@" 
+nightwatch "$@"
+RESULT=$(echo $?)
 cp -R /home/node/videos/* /opt/node/videos/ 2>/dev/null
 cp -R /home/node/screenshots/* /opt/node/screenshots/ 2>/dev/null
+exit ${RESULT}
