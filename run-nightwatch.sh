1676e1c7fa4d81a81e023564d26ef9582acc60fd
Marc Carné <marc.carne@werlabs.com>
2019-03-13 10:57:55 +0100

    Try to remove videos already in the working dir.

--- run-nightwatch.sh
+++ run-nightwatch.sh
@@ -1,5 +1,8 @@
 rm -rf /home/node/videos/*
 rm -rf /home/node/screenshots/*
+rm -rf /opt/node/videos/*
+rm -rf /opt/node/screenshots/*
+
 nightwatch "$@"
 RESULT=$(echo $?)
 cp -R /home/node/videos/* /opt/node/videos/ 2>/dev/null
